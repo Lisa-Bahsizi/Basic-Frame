52b9aa589ae4851442ab44878bfd3f8b9533bbc3
Lisa-Bahsizi <lisabashizi28@gmail.com>
2024-08-05 01:57:56 -0700

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,18 @@
+
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MyFirstFrame myFrame = new MyFirstFrame();
+    }
+    
+}
